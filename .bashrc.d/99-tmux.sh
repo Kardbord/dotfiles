73dad26d8934753cbc5f7631fe045bb5247e8371
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-07-22 13:13:33 -0700

    don't kill shell if tmux fails

--- .bashrc.d/99-tmux.sh
+++ .bashrc.d/99-tmux.sh
@@ -2,4 +2,6 @@ if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] &&
   export TMUX_TMPDIR="${HOME}/.local/run/"
   mkdir -p "${TMUX_TMPDIR}"
   exec tmux -u
+  # Only reached if exec fails
+  echo "[tmux] failed to start, continuing without tmux" >&2
 fi

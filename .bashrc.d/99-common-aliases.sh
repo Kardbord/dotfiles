394d4816132d5807f92544b4f9bf24b2e615d8b5
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-07-22 13:11:43 -0700

    Fixed aliases

--- .bashrc.d/99-common-aliases.sh
+++ .bashrc.d/99-common-aliases.sh
@@ -1,6 +1,10 @@
-alias ll='ls -alh'
+if ls --color=auto &>/dev/null 2>&1; then
+  alias ll='ls -alFh --color=auto'
+else
+  alias ll='ls -alFhG'
+fi
 
 if command -v rg &>/dev/null; then
   alias rg='rg -g "!.git/*" --hidden -n'
-  alias rgi='rg -iglob "!.git/*" --hidden -ni'
+  alias rgi='rg -i -g "!.git/*" --hidden -n'
 fi

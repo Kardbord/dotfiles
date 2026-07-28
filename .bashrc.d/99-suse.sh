6aa56531e5878bf20e56ccf33d89577058135939
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-07-28 16:39:56 -0700

    Update system flatpaks as root

--- .bashrc.d/99-suse.sh
+++ .bashrc.d/99-suse.sh
@@ -1,3 +1,3 @@
 [[ "${OS_ID}" = opensuse-* ]] || return
 
-alias update='sudo sh -c "zypper up -y && zypper dup -y" && flatpak --user update -y && flatpak --system update -y'
+alias update='sudo sh -c "zypper up -y && zypper dup -y && flatpak --system update -y" && flatpak --user update -y'

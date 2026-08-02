84c1cea8a8d540af1d4a1807d16eafb5d6cc6032
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-08-01 21:22:23 -0700

    improve wsl-keyring error messages

--- .bashrc.d/97-wsl-keyring.sh
+++ .bashrc.d/97-wsl-keyring.sh
@@ -3,10 +3,15 @@
 if [[ -z "${DBUS_SESSION_BUS_ADDRESS:-}" ]]; then
   if command -v dbus-launch &>/dev/null; then
     eval "$(dbus-launch --sh-syntax)" &>/dev/null
+  else
+    echo "[dbus] dbus is not installed (see https://www.freedesktop.org/wiki/Software/dbus/)" >&2
   fi
 fi
 
-if [[ -n "${DBUS_SESSION_BUS_ADDRESS:-}" ]] && \
-  command -v gnome-keyring-daemon &>/dev/null; then
-  gnome-keyring-daemon --start --components=secrets &>/dev/null
+if [[ -n "${DBUS_SESSION_BUS_ADDRESS:-}" ]]; then
+  if command -v gnome-keyring-daemon &>/dev/null; then
+    gnome-keyring-daemon --start --components=secrets &>/dev/null
+  else
+    echo "[wsl-secrets] gnome-keyring-daemon is not installed (see https://gitlab.gnome.org/GNOME/gnome-keyring/)" >&2
+  fi
 fi

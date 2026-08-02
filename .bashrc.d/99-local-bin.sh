17345fe3df9938acf6e77fdc74b2cb3f797613e5
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-08-01 23:54:26 -0700

    Check that home bin folders are owned by user before prepending to PATH

--- .bashrc.d/99-local-bin.sh
+++ .bashrc.d/99-local-bin.sh
@@ -1,7 +1,7 @@
-if [[ -d "${HOME}/bin" ]]; then
+if [[ -d "${HOME}/bin" && -O "${HOME}/bin" ]]; then
   export PATH="${HOME}/bin:${PATH}"
 fi
 
-if [[ -d "${HOME}/.local/bin" ]]; then
+if [[ -d "${HOME}/.local/bin" && -O "${HOME}/.local/bin" ]]; then
   export PATH="${HOME}/.local/bin:$PATH"
 fi

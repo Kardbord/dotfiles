4ce00cd9e3193da11ff86f7f2dd13dcb89cc0bb6
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-08-06 12:15:41 -0700

    Add gopath to PATH

--- .bashrc.d/99-go.sh
+++ .bashrc.d/99-go.sh
@@ -1,3 +1,12 @@
 if [[ -d "/usr/local/go/bin" ]]; then
   export PATH="${PATH}:/usr/local/go/bin"
 fi
+
+if [[ -d "${GOBIN}" ]]; then
+  export PATH="${PATH}:${GOBIN}"
+fi
+
+if [[ -d "${GOPATH:-${HOME}/go}/bin" ]]; then
+  export PATH="${PATH}:${GOPATH:-${HOME}/go}/bin"
+fi
+

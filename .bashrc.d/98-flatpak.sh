3ac7d600e122c0a9884169b76916fcf5bb07827f
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-08-20 12:47:12 -0700

    Launch dbus with WSL

--- .bashrc.d/98-flatpak.sh
+++ .bashrc.d/98-flatpak.sh
@@ -1,5 +1,16 @@
+declare -A _REQUIRED_FLATPAK_REMOTES=(
+  [https://dl.flathub.org/repo]=https://dl.flathub.org/repo/flathub.flatpakrepo
+  [oci+https://kardbord.github.io/Boxes]=https://kardbord.github.io/Boxes/kardbord-boxes.flatpakrepo
+)
+
 _REQUIRED_FLATPAKS=(
   "org.freedesktop.Sdk"
+  "io.github.kardbord.Sdk"
+  "io.github.kardbord.Platform"
+  "io.github.kardbord.neovim"
+  "io.github.kardbord.opencode"
+  "io.github.kardbord.ripgrep"
+  "io.github.kardbord.fd"
 )
 
 _REQUIRED_FLATPAK_SDK_EXTS=(
@@ -13,13 +24,25 @@ _REQUIRED_FLATPAK_SDK_EXTS=(
 )
 
 # Useful for flatpaks that use the FLATPAK_ENABLE_SDK_EXT environment variable, such as neovim.
-_FLATPAK_ENABLE_SDK_EXT="$(IFS=,; echo "${_REQUIRED_FLATPAK_SDK_EXTS[*]/org.freedesktop.Sdk.Extension./}")"
+_FLATPAK_ENABLE_SDK_EXT="$(
+  IFS=,
+  echo "${_REQUIRED_FLATPAK_SDK_EXTS[*]/org.freedesktop.Sdk.Extension./}"
+)"
 
 _ensure_flatpak() {
   if ! command -v flatpak &>/dev/null; then
     echo "[sandbox] flatpak is not installed (see https://flatpak.org)" >&2
     return 1
   fi
+
+  for remote in "${!_REQUIRED_FLATPAK_REMOTES[@]}"; do
+    if ! flatpak remotes --columns=url | grep "${remote}"; then
+      echo "[sandbox] flatpak is missing a required remote: ${remote}" >&2
+      echo "[sandbox] Install with:" >&2
+      echo "  flatpak remote-add --user --if-not-exists <name> ${_REQUIRED_FLATPAK_REMOTES[${remote}]}"
+      return 1
+    fi
+  done
 
   for dep in "${_REQUIRED_FLATPAKS[@]}"; do
     if ! flatpak info "${dep}" &>/dev/null; then
--- .bashrc.d/99-wsl.sh
+++ .bashrc.d/99-wsl.sh
@@ -1,3 +1,5 @@
 [[ -n "${_WSL}" ]] || return
 
 alias exit='find /tmp -type f -atime +10 -delete && exit'
+
+eval "$(dbus-launch --sh-syntax 2>/dev/null)"

89af9b1f1d62ce08f81438cb3d28a8ded9f9da15
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-08-21 12:36:02 -0700

    Updated required flatpaks

--- .bashrc.d/98-flatpak.sh
+++ .bashrc.d/98-flatpak.sh
@@ -7,10 +7,17 @@ _REQUIRED_FLATPAKS=(
   "org.freedesktop.Sdk"
   "io.github.kardbord.Sdk"
   "io.github.kardbord.Platform"
+  "io.github.kardbord.fd"
+  "io.github.kardbord.fzf"
   "io.github.kardbord.neovim"
   "io.github.kardbord.opencode"
   "io.github.kardbord.ripgrep"
-  "io.github.kardbord.fd"
+  "io.github.kardbord.sk"
+  "io.github.kardbord.tool.lua"
+  "io.github.kardbord.tool.ruby4"
+  "io.github.kardbord.tool.uv"
+  "io.github.kardbord.treesitter-cli"
+  "io.github.kardbord.viu"
 )
 
 _REQUIRED_FLATPAK_SDK_EXTS=(
@@ -19,8 +26,10 @@ _REQUIRED_FLATPAK_SDK_EXTS=(
   "org.freedesktop.Sdk.Extension.llvm22"
   "org.freedesktop.Sdk.Extension.mingw-w64"
   "org.freedesktop.Sdk.Extension.node26"
-  "org.freedesktop.Sdk.Extension.openjdk25"
+  "org.freedesktop.Sdk.Extension.openjdk"
   "org.freedesktop.Sdk.Extension.rust-stable"
+  "org.freedesktop.Sdk.Extension.typescript"
+  "org.freedesktop.Sdk.Extension.bazel"
 )
 
 # Useful for flatpaks that use the FLATPAK_ENABLE_SDK_EXT environment variable, such as neovim.
@@ -46,14 +55,14 @@ _ensure_flatpak() {
 
   for dep in "${_REQUIRED_FLATPAKS[@]}"; do
     if ! flatpak info "${dep}" &>/dev/null; then
-      echo "[sandbox] ${dep} is not installed via flatpak (see https://flathub.org/en/apps/${dep})" >&2
+      echo "[sandbox] ${dep} is not installed via flatpak." >&2
       return 1
     fi
   done
 
   for dep in "${_REQUIRED_FLATPAK_SDK_EXTS[@]}"; do
     if ! flatpak info "${dep}" &>/dev/null; then
-      echo "[sandbox] ${dep} is not installed via flatpak (see https://flathub.org/en/apps/${dep})" >&2
+      echo "[sandbox] ${dep} is not installed via flatpak." >&2
       return 1
     fi
   done
